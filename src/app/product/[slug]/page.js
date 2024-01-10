b11c8ea3084d03e0526bf586cb2044cd67d5b452
Noah Rodgers <noah@createdbynoah.com>
2024-01-10 01:45:11 -0600

    Add product details page component

---
+++ src/app/product/[slug]/page.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiFillStar,
+  AiOutlineStar,
+} from 'react-icons/ai';
+
+import { getProduct, getAllProducts } from '../../../lib/client';
+import { Product } from '@/components';
+
+const ProductDetails = async ({ params }) => {
+  const { name, price, description, imageUrls } = await getProduct(params.slug);
+  const products = await getAllProducts();
+
+  return (
+    <div>
+      <div className="product-detail-container">
+        <div className="image-container">
+          <img src={imageUrls.length !== 0 && imageUrls[0]} alt={name} />
+        </div>
+        {/* <div className="small-images-container">
+          {imageUrls.map((imageUrl, index) => (
+            <img src={imageUrl} className="" onMouseEnter="" />
+          ))}
+
+        </div> */}
+        <div className="product-detail-desc">
+          <h1>{name}</h1>
+          <div className="reviews">
+            <div>
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiFillStar />
+              <AiOutlineStar />
+            </div>
+            <p>(20)</p>
+          </div>
+          <h4>Details: </h4>
+          <p>{description}</p>
+          <p className="price">${price}</p>
+          <div className="quantity">
+            <h3>Quantity:</h3>
+            <p className="quantity-desc">
+              <span className="minus" onClick="">
+                <AiOutlineMinus />
+              </span>
+              <span className="num" onClick="">
+                0
+              </span>
+              <span className="plus" onClick="">
+                <AiOutlinePlus />
+              </span>
+            </p>
+          </div>
+          <div className="buttons">
+            <button type="button" className="add-to-cart" onClick="">
+              Add to Cart
+            </button>
+            <button type="button" className="buy-now" onClick="">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container">
+            {products.map((product) => (
+              <Product key={product._id} product={product} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
