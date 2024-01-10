b5a45963d1df9502970980e88a851e88045d10e4
Noah Rodgers <noah@createdbynoah.com>
2024-01-10 14:43:00 -0600

    Update cart item quantity in Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
+'use client';
 import React from 'react';
 import Link from 'next/link';
 import { AiOutlineShopping } from 'react-icons/ai';
 
+import { useStateContext } from '@/context/StateContext';
+
 const Navbar = () => {
+  const { totalItems } = useStateContext();
   return (
     <div className="navbar-container">
       <p className="logo">
@@ -10,7 +14,7 @@ const Navbar = () => {
       </p>
       <button type="button" className="cart-icon" onClick="">
         <AiOutlineShopping />
-        <span className="cart-item-qty">3</span>
+        <span className="cart-item-qty">{totalItems}</span>
       </button>
     </div>
   );
