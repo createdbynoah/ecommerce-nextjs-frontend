60113106ae040a5136b8e0d9b253349f0fd0249b
Noah Rodgers <noah@createdbynoah.com>
2024-01-11 16:38:13 -0600

    Delete unused code and refactor imports

--- src/app/product/[slug]/page.js
+++ src/app/product/[slug]/page.js
@@ -1,25 +1,17 @@
 import React from 'react';
 
-import { getProduct, getAllProducts } from '../../../lib/client';
-import {
-  ImageGallery,
-  Product,
-  ProductDetails,
-  ProductButtons,
-  ProductQuantity,
-  ProductMarquee,
-} from '@/components';
+import { getProduct, getAllProducts } from '@/lib/client';
+import { ProductDetails, ProductMarquee } from '@/components';
 
 const ProductPage = async ({ params }) => {
   const product = await getProduct(params.slug);
-  const { name, price, description, imageUrls } = product;
   const products = await getAllProducts();
 
   return (
-    <div>
+    <>
       <ProductDetails product={product} products={products} />
       <ProductMarquee products={products} />
-    </div>
+    </>
   );
 };
 
