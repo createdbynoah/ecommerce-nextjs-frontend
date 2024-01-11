fcfcabf1da8353f05488f2801ed192a93ef33296
Noah Rodgers <noah@createdbynoah.com>
2024-01-11 16:33:47 -0600

    Add OrderComplete component and celebrate function

---
+++ src/components/OrderComplete.jsx
@@ -0,0 +1,44 @@
+'use client';
+import React, { useState, useEffect, use } from 'react';
+import Link from 'next/link';
+import { BsBagCheckFill } from 'react-icons/bs';
+
+import { useStateContext } from '@/context/StateContext';
+import { celebrate } from '@/lib/utils';
+
+const OrderComplete = () => {
+  const { setCartItems, setTotalItems, setTotalPrice } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalItems(0);
+    setTotalPrice(0);
+    celebrate();
+  }, []);
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for your receipt.</p>
+        <p className="description">
+          If you have any questions, please email{' '}
+          <a className="email" href="mailto:order@example.com">
+            order@example.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default OrderComplete;
