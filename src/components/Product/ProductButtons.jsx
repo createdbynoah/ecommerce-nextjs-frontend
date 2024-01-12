1df63b94b55e6270a866ebc9fc80e7e4acfa0ecf
Noah Rodgers <noah@createdbynoah.com>
2024-01-12 16:51:41 -0600

    Refactor ProductButtons component to disable buttons when product is out of stock

--- src/components/Product/ProductButtons.jsx
+++ src/components/Product/ProductButtons.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 
 const ProductButtons = ({ product, addToCart, quantity, handleBuyNow }) => {
+  const { outOfStock } = product;
   return (
     <div className="buttons">
       <button
         type="button"
-        className="add-to-cart"
+        className={`add-to-cart ${outOfStock ? 'disabled' : ''}`}
         onClick={() => addToCart(product, quantity)}
+        disabled={outOfStock}
       >
         Add to Cart
       </button>
-      <button type="button" className="buy-now" onClick={handleBuyNow}>
+      <button
+        type="button"
+        className={`buy-now ${outOfStock ? 'disabled' : ''}`}
+        onClick={handleBuyNow}
+        disabled={outOfStock}
+      >
         Buy Now
       </button>
     </div>
