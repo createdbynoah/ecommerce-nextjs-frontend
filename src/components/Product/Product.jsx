e2b5b11c24401c61eafb367ffd77bedafb47c24a
Noah Rodgers <noah@createdbynoah.com>
2024-01-12 02:05:24 -0600

    Refactor Product component to include add to cart functionality

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,23 +1,77 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Link from 'next/link';
+import {
+  TbShoppingBagPlus,
+  TbShoppingBagCheck,
+  TbShoppingBagX,
+} from 'react-icons/tb';
+import { LuTrash2 } from 'react-icons/lu';
 
-const Product = ({ product: { imageUrls, name, slug, price } }) => {
+import { useStateContext } from '@/context/StateContext';
+
+const Product = ({ product }) => {
+  const { onAddToCart, cartItems, onRemoveFromCart } = useStateContext();
+  const [isHovered, setIsHovered] = useState(false);
+
+  const { imageUrls, name, slug, price } = product;
+
+  const handleAddToCart = (product) => {
+    if (cartItems.find((item) => item._id === product._id)) {
+      onRemoveFromCart(product);
+    } else {
+      onAddToCart(product);
+    }
+  };
+
   return (
     <div>
-      <Link href={`/product/${slug.current}`}>
-        <div className="product-card">
-          <img
-            src={imageUrls && imageUrls.length !== 0 && imageUrls[0].url}
-            alt={name}
-            width={250}
-            height={250}
-            className="product-image"
-          />
-          <p className="product-name">{name}</p>
-          <p className="product-price">${price}</p>
-          <div className="product-card-info"></div>
+      <div className="product-card">
+        <Link href={`/product/${slug.current}`}>
+          <div className="row image-container">
+            <img
+              src={imageUrls && imageUrls.length !== 0 && imageUrls[0].url}
+              alt={name}
+              width={250}
+              height={250}
+              className="product-image"
+            />
+          </div>
+        </Link>
+        <div className="row">
+          <Link href={`/product/${slug.current}`}>
+            <div className="col">
+              <p className="product-name ">{name}</p>
+              <p className="product-price">${price}</p>
+            </div>
+          </Link>
+          <div
+            className={`col btn-add ${
+              cartItems.find((item) => item._id === product._id)
+                ? isHovered
+                  ? 'hovered'
+                  : 'added'
+                : 'not-added'
+            }`}
+          >
+            <button
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              onClick={() => handleAddToCart(product)}
+            >
+              {cartItems.find((item) => item._id === product._id) ? (
+                isHovered ? (
+                  <TbShoppingBagX />
+                ) : (
+                  <TbShoppingBagCheck />
+                )
+              ) : (
+                <TbShoppingBagPlus />
+              )}
+            </button>
+          </div>
         </div>
-      </Link>
+      </div>
     </div>
   );
 };
